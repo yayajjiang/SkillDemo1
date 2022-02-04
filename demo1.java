f8fe0a1753c0fe4561b51e71850a101748a47e5a
jinya jiang <60903767+yayajjiang@users.noreply.github.com>
2022-02-04 15:39:14 -0800

    fail test

--- demo1.java
+++ demo1.java
@@ -1,2 +1,5 @@
 public class demo1{
+    public static int multiple(int a, int b){
+        return a*b;
+    }
 }
---
+++ demo1test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class demo1test {
+    @Test
+    public void addition() {
+        assertEquals(2,demo1.multiple(1,4));//make it fail
+    }
+}
